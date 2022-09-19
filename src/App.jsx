995517fe8c8af7d6c1f695be4c3639c7462e84d2
Malka <malkaanjum1999@gmail.com>
2022-09-19 23:36:15 +0530

    adding input is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import TodoLists from "./TodoLists";
 
 const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  const handleLists = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
   return (
     <>
-      <div className='main_div'>
-        <div className='center_div'>
-          <br/>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
           <h1>ToDo List</h1>
-          <br/>
-          <input type='text' placeholder='Add an Item' />
-          <button>+</button>
+          <br />
+          <input
+            type="text"
+            placeholder="Add an Item"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={handleLists}> + </button>
 
           <ol>
-            <li>buy apple</li>
+            {/* <li>{inputList}</li> */}
+            {items.map((item) => {
+              return <TodoLists text={item}></TodoLists>
+            })}
           </ol>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
